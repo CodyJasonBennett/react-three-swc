8aab0a2e7d36b3deea96c7f811bb6f79cdcb76e4
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2023-05-19 02:23:32 -0500

    refactor: try to flag extend calls

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,22 @@
-use swc_core::ecma::ast::{ CallExpr, Program };
-use swc_core::ecma::visit::{ as_folder, FoldWith, VisitMut };
+use swc_core::common::util::take::Take;
+use swc_core::ecma::ast::{ CallExpr, Expr, Program };
+use swc_core::ecma::visit::{ VisitMut, FoldWith, as_folder };
 use swc_core::ecma::transforms::testing::test;
 use swc_core::plugin::{ plugin_transform, proxies::TransformPluginProgramMetadata };
 
 pub struct ReactThreeVisitor;
 
 impl VisitMut for ReactThreeVisitor {
-    fn visit_mut_call_expr(&mut self, n: &mut CallExpr) {}
+  fn visit_mut_call_expr(&mut self, node: &mut CallExpr) {
+    // Invalidate node
+    node.take();
+  }
+
+  // TODO: not called for CallExpr
+  fn visit_mut_exprs(&mut self, nodes: &mut Vec<Box<Expr>>) {
+    // Remove all invalid nodes
+    nodes.retain(|node| !node.is_invalid());
+  }
 }
 
 #[plugin_transform]
@@ -14,6 +24,7 @@ pub fn transform(program: Program, _metadata: TransformPluginProgramMetadata) ->
   program.fold_with(&mut as_folder(ReactThreeVisitor))
 }
 
+// removes namespaced extend calls
 test!(
   Default::default(),
   |_| as_folder(ReactThreeVisitor),
@@ -28,6 +39,8 @@ test!(
     import * as THREE from "three";
     import { extend } from "@react-three/fiber";
 
-    extend(THREE);
+    // TODO: take() assigns a dummy callee which is super() here.
+    // Is this a bug in SWC? This isn't valid at the top-level.
+    super();
   "#
 );
